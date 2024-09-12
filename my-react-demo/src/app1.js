3d4475919f13fb1b2a4e6f96a81a3fe0a00acdc9
Mike Stockman <michaelstockman@ucwv.edu>
2024-09-12 17:02:21 -0400

    updated app1.js

---
+++ my-react-demo/src/app1.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addItem = () => {
+    if (inputValue.trim() !== '') {
+      setItems([...items, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Enter an item"
+      />
+      <button onClick={addItem}>Add</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
