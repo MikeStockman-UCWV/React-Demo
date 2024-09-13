c01c02e9588783a38a3383bad584e92571e493f6
Mike Stockman <michaelstockman@ucwv.edu>
2024-09-12 23:06:18 -0400

    updated html

--- my-react-demo/src/store.js
+++ my-react-demo/src/store.js
@@ -1,5 +1,5 @@
 // src/store.js
-import { createStore } from 'redux';
+import { configureStore } from 'redux';
 
 const initialState = {
   count: 0,
@@ -16,5 +16,5 @@ function counterReducer(state = initialState, action) {
   }
 }
 
-const store = createStore(counterReducer);
+const store = configureStore(counterReducer);
 export default store;
